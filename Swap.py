8c2f9a7c5cb73398624525eafdbb08bb67747bf0
charudeshna <76227078+charudeshna@users.noreply.github.com>
2022-09-30 12:45:57 +0530

    Add files via upload

---
+++ Swap.py
@@ -0,0 +1,50 @@
+''' SWAPPING OF TWO NUMBERS WITH DIFFERENT METHODS '''
+
+n1 = int(input("enter the value"))
+n2 = int(input("enter the value"))
+print(n1,n2)
+n1,n2 = n2,n1
+print(n1,n2)
+
+n1 = int(input("enter the value"))
+n2 = int(input("enter the value"))
+print(n1,n2)
+n3 = n1
+n1 = n2
+n2 = n3
+print(n1,n2)
+
+
+n1 = int(input("enter the value"))
+n2 = int(input("enter the value"))
+print(n1,n2)
+n1 = n1 + n2
+n2 = n1 - n2
+n1 = n1 - n2
+print(n1,n2) 
+
+
+n1 = int(input("enter the value"))
+n2 = int(input("enter the value"))
+print(n1,n2)
+n1 = n1 * n2
+n2 = n1 // n2
+n1 = n1 // n2
+print(n1,n2)
+
+
+n1 = int(input("enter the value"))
+n2 = int(input("enter the value"))
+print(n1,n2)
+n1 = n1 ^ n2
+n2 = n1 ^ n2
+n1 = n1 ^ n2
+print(n1,n2)
+
+
+
+
+
+
+
+
